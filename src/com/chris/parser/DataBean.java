c3940e791319a80ed33c7aa576bf7da9392c34dd
Cjohnson187 <chrisfjohnson3@gmail.com>
2020-12-05 15:39:40 -0700

    comitting finished buffer and stream parsers but still needs cleaning

---
+++ src/com/chris/parser/DataBean.java
@@ -0,0 +1,125 @@
+package com.chris.parser;
+
+import java.io.Serializable;
+
+/****************************************************************************
+ * <b>Title</b>: dataBean.java
+ * <b>Project</b>: FileParser
+ * <b>Description: </b> Bean to store each line of the file.
+ * <b>Copyright:</b> Copyright (c) 2020
+ * <b>Company:</b> Silicon Mountain Technologies
+ * 
+ * @author Chris Johnson
+ * @version 1.0
+ * @since Dec 2, 2020
+ * @updates:
+ ****************************************************************************/
+
+public class DataBean implements Serializable {
+	private String date;
+	private String time;
+	private String type;
+	private String from;
+	private String to;
+	
+	/**
+	 * Empty constructor for file data bean
+	 */
+	public DataBean() {
+	}
+	/**
+	 * Date setter.
+	 * @param date
+	 */
+	public void setDate(String date) {
+		this.date = date;
+	}
+	/**
+	 * Time setter.
+	 * @param time
+	 */
+	public void setTime(String time) {
+		this.time = time;
+	}
+	/**
+	 * Type setter.
+	 * @param type
+	 */
+	public void setType(String type) {
+		this.type = type;
+	}
+	/**
+	 * From setter.
+	 * @param from
+	 */
+	public void setFrom(String from) {
+		this.from = from;
+	}
+
+	/**
+	 * To setter.
+	 * @param to
+	 */
+	public void setTo(String to) {
+		this.to = to;
+	}
+	/**
+	 * Date getter.
+	 * @return
+	 */
+	public String getDate() {
+		return date;
+	}
+	/**
+	 * Time getter.
+	 * @return
+	 */
+	public String getTime() {
+		return time;
+	}
+	/**
+	 * Type getter.
+	 * @return
+	 */
+	public String getType() {
+		return type;
+	}
+	/**
+	 * From getter.
+	 * @return
+	 */
+	public String getFrom() {
+		return from;
+	}
+	/**
+	 * To getter.
+	 * @return
+	 */
+	public String getTo() {
+		return to;
+	}
+	/**
+	 * Method to split each line from file reader and store each segment in the bean.
+	 * Not safe if file changes format or data it contains
+	 * @param line
+	 */
+	public void parse(String line) {
+		String[] lineArray = line.split("\\s"); //  \\s = split by tab and space
+		// could maybe sanitize data
+		setDate(lineArray[0]);
+		setTime(lineArray[1]);
+		setType(lineArray[2]);
+		setFrom(lineArray[3]);
+		// skipping arrow character between from and to
+		setTo(lineArray[5]);
+	}
+	/**
+	 *Override toString method for data bean.
+	 */
+	@Override
+	public String toString() {
+		return "Date- " + date + ", Time- " + time + " Type- " + type + ", From- " + from + ", To- " + to + " ";
+	}
+
+
+}
---
+++ src/com/chris/parser/StreamParser.java
@@ -0,0 +1,102 @@
+package com.chris.parser;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+
+
+
+/****************************************************************************
+ * <b>Title</b>: StreamParser.java
+ * <b>Project</b>: FileParser
+ * <b>Description: </b> The parser class will create a bean for each line 
+ * 		from the input stream and store them in an array before writing using an 
+ * 		output stream.
+ * <b>Copyright:</b> Copyright (c) 2020
+ * <b>Company:</b> Silicon Mountain Technologies
+ * 
+ * @author Chris Johnson
+ * @version 1.0
+ * @since Dec 2, 2020
+ * @updates:
+ ****************************************************************************/
+public class StreamParser {
+	
+	/**
+	 * Main method to make make an input stream to read the file and later make an out put stream to write the data to a new file.
+	 * @param args
+	 * @throws IOException
+	 */
+	public static void main(String[] args) throws IOException {
+		// carriage return  = 13 int/ 0D hex
+		// space 			= 32 int/ 20 hex
+		// tab				= 9 int
+		
+		String inFile = "src/com/chris/input/master";
+		String outFile = "outPutStreamed";
+		
+		ArrayList<DataBean> beanList;
+		
+		StreamParser stream = new StreamParser();
+		
+		beanList = stream.processIn(inFile);
+		
+		stream.processOut(beanList, outFile);
+	}
+
+	/**
+	 * Method to create the input stream and read each segment of bytes to store into a data bean and data bean to list.
+	 * @param inFile
+	 */
+	public ArrayList<DataBean> processIn(String inFile) throws IOException {
+		FileInputStream inputStream = new FileInputStream(inFile);
+		ArrayList<DataBean> beanList = new ArrayList<DataBean>();
+		try {
+			int currentByte;
+			String line = "";
+			DataBean bean = new DataBean();
+			while ((currentByte = inputStream.read()) != -1) {
+				if(currentByte == 10) { 
+					bean.parse(line); 
+					beanList.add(bean);
+					line = ""; 
+					continue;
+				}
+				line += ((char) currentByte); //convert to char
+			}
+		}
+		catch (FileNotFoundException ex){
+			System.out.println("Error writing to file- '" + inFile + "'" );
+		}
+		finally {
+			inputStream.close();
+		}
+		return beanList;
+	}
+	
+	/**
+	 * Method to read each bean from the bean list and and print them to a file while reading.
+	 * @param beanList
+	 * @param fileName
+	 */
+	public void processOut (ArrayList<DataBean> beanList, String fileName) throws IOException {
+		FileOutputStream outStream = null;
+		final byte[] lineBreak = {10};
+		try {
+			outStream = new FileOutputStream(fileName);
+			for (DataBean bean : beanList ) {
+				byte[] buffer = (bean.toString()).getBytes(); 
+				outStream.write(buffer);
+				outStream.write(lineBreak);
+			}
+		}
+		catch (IOException ex) {
+			System.out.println("Error writing to file- '" + fileName + "'" );
+		}
+		finally {
+			outStream.close();
+		}
+	}
+}
